9eeb3262f8bf78f3f356b8c79de8bc8ea816cfbf
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-11-01 14:11:34 -0300

    v2.4

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -20,11 +20,12 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
+        color="rgba(128, 0, 128, 0.5)"
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading
+      />
+
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
