e0d3fa38c7e1aaf4f8b998c47d0e9d7fa8a22b43
Denis W. Bonaccini <114507650+bonacciniWd@users.noreply.github.com>
2023-11-01 17:22:09 -0300

    v2.2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Wpp from "./components/Wpp.jsx";
 import Sliding from "./components/Sliding.jsx";
+import Analythics from "./components/Analythics.jsx";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
        <Hero />
       </div>
       <Wpp />
+      <Analythics />
     </BrowserRouter>
   );
 }
