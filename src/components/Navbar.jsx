91eb13e77c01994794840e48a1454aa1e72f3bfd
Denis Bonaccini <dbonaccini8@gmail.com>
2024-09-20 16:25:42 -0300

    upstream

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
-import { logo, brazilFlag, usaFlag, italyFlag, franceFlag, spainFlag } from "../assets";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-  const [currentFlag, setCurrentFlag] = useState(brazilFlag);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      setScrolled(scrollTop > 100);
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
+
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-  const handleLanguageChange = (flag) => {
-    setCurrentFlag(flag); // Atualiza a bandeira para o idioma selecionado
-    setToggle(false); // Fecha o menu após selecionar um idioma
-  };
 
   return (
     <nav
@@ -37,7 +37,10 @@ const Navbar = () => {
         <Link
           to='/'
           className='flex items-center gap-2'
-          onClick={() => window.scrollTo(0, 0)}
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
           <img
             src={logo}
@@ -51,50 +54,6 @@ const Navbar = () => {
           </p>
         </Link>
 
-        {/* Menu de bandeiras no lugar do menu de hamburguer */}
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img
-            src={currentFlag}  // Bandeira atual
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain cursor-pointer'
-            onClick={() => setToggle(!toggle)}
-          />
-
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 bg-slate-900 border-2 border-indigo-700 bg-opacity-90 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-              <li
-                className='font-poppins font-medium cursor-pointer text-[16px] flex items-center gap-2'
-                onClick={() => handleLanguageChange(usaFlag)}
-              >
-                <img src={usaFlag} alt='USA Flag' className='w-6 h-6' /> EUA
-              </li>
-              <li
-                className='font-poppins font-medium cursor-pointer text-[16px] flex items-center gap-2'
-                onClick={() => handleLanguageChange(italyFlag)}
-              >
-                <img src={italyFlag} alt='Italy Flag' className='w-6 h-6' /> Itália
-              </li>
-              <li
-                className='font-poppins font-medium cursor-pointer text-[16px] flex items-center gap-2'
-                onClick={() => handleLanguageChange(franceFlag)}
-              >
-                <img src={franceFlag} alt='France Flag' className='w-6 h-6' /> França
-              </li>
-              <li
-                className='font-poppins font-medium cursor-pointer text-[16px] flex items-center gap-2'
-                onClick={() => handleLanguageChange(spainFlag)}
-              >
-                <img src={spainFlag} alt='Spain Flag' className='w-6 h-6' /> Espanha
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        {/* Menu padrão para desktop */}
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((nav) => (
             <li
@@ -108,6 +67,38 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
